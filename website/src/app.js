bca6a38968a6ca023a0796bc201203ecac769fc5
Alfonce Morara <alfoncemorara412@gmail.com>
2025-09-12 15:36:52 +0300

    Update app.js
    Signed-off-by: Alfonce Morara <alfoncemorara412@gmail.com>

--- website/src/app.js
+++ website/src/app.js
@@ -1,13 +1,22 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
+    const controls = document.querySelectorAll(".control");
+    const sections = document.querySelectorAll(".container");
+
+    controls.forEach(button => {
         button.addEventListener("click", function() {
+            // Toggle active button
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+
+            // Toggle active section
+            document.querySelector(".container.active").classList.remove("active");
+            document.getElementById(this.dataset.id).classList.add("active");
+        });
     });
+
+    // Theme toggle
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-    })
+    });
 })();
+
